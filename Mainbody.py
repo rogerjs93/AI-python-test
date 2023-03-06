20f327d8635daafa702cd6e445959fdf456ccca2
Roger Salas <58913883+rogerjs93@users.noreply.github.com>
2023-03-06 08:52:26 +0200

    Update Mainbody.py

--- Mainbody.py
+++ Mainbody.py
@@ -1,1 +1,26 @@
+import tensorflow as tf
+from tensorflow import keras
 
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+
+# Define the model architecture
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(x_test, y_test, verbose=2)
+print('\nTest accuracy:', test_acc)
